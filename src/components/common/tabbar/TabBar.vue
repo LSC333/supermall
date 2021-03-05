286a2edd9776ee255d6e815b6dab46f63aa05f7f
LSC333 <2668304028@qq.com>
2021-03-05 16:58:20 +0800

    'tarbar'

---
+++ src/components/common/tabbar/TabBar.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="tab-bar">
+     <slot></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "TabBar"
+  }
+</script>
+
+<style scoped>
+
+    /*css引用加@*/
+  @import "~assets/css/base.css";
+
+  #tab-bar {
+    display: flex;
+    background-color: #f6f6f6;
+    position: fixed;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    box-shadow: 0px -1px 5px rgba(10,10,10,0.1);
+  }
+
+
+</style>
---
+++ src/components/content/mainTabbar/MainTabBar.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <tab-bar>
+      <tab-bar-item path="/home">
+        <img slot="item-icon" src="~assets/img/home/earth-outline.svg" alt="">
+        <img slot="item-icon-active" src="~assets/img/home/earth.svg" alt="">
+        <div slot="item-text">首页</div>
+      </tab-bar-item>
+      <tab-bar-item path="/category">
+        <img slot="item-icon" src="~assets/img/category/apps-outline.svg" alt="">
+        <img slot="item-icon-active" src="~assets/img/category/apps.svg" alt="">
+        <div slot="item-text">分类</div>
+      </tab-bar-item>
+      <tab-bar-item path="/cart">
+        <img slot="item-icon" src="~assets/img/cart/cart-outline.svg" alt="">
+        <img slot="item-icon-active" src="~assets/img/cart/cart.svg" alt="">
+        <div slot="item-text">购物车</div>
+      </tab-bar-item>
+      <tab-bar-item path="/profile">
+        <img slot="item-icon" src="~assets/img/profile/person-outline.svg" alt="">
+        <img slot="item-icon-active" src="~assets/img/profile/person.svg" alt="">
+        <div slot="item-text">我的</div>
+      </tab-bar-item>
+    </tab-bar>
+  </div>
+</template>
+
+<script>
+
+  import TabBar from "components/common/tabbar/TabBar";
+  import TabBarItem from "components/common/tabbar/TabBarItem";
+
+  export default {
+    name: "MainTarBar",
+    components:{
+      TabBar,
+      TabBarItem
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
